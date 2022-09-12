4f48b683b384665179615237d255645d150c68e3
Daniel Ekman <knegge@gmail.com>
2022-09-12 22:10:29 +0200

    Add ENV variables to start script
    Signed-off-by: Daniel Ekman <knegge@gmail.com>

--- up.sh
+++ up.sh
@@ -6,11 +6,11 @@ if ! docker volume inspect satnogs-auto-scheduler-cache > /dev/null; then
 	docker run --rm -u 0:0 -v satnogs-auto-scheduler-cache:/tmp/cache/ -it satnogs-auto-scheduler chown -R satnogs:satnogs /tmp/cache/
 fi
 source .env
-docker run --name satnogs-auto-scheduler \
+docker run -d --name satnogs-auto-scheduler \
 	-e SATNOGS_STATION_ID="${SATNOGS_STATION_ID}" \
 	-e SATNOGS_SCHEDULE_EXTRA="${SATNOGS_SCHEDULE_EXTRA}" \
 	-e SATNOGS_API_TOKEN="${SATNOGS_API_TOKEN}" \
 	-e SATNOGS_DB_API_TOKEN="${SATNOGS_DB_API_TOKEN}" \
 	-v satnogs-auto-scheduler-cache:/tmp/cache/ \
-	-it satnogs-auto-scheduler "$@"
+	satnogs-auto-scheduler "$@"
 
