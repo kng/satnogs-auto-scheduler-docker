6bcf63af9f5605c73cb1eeb496c0a27b4cb18223
Daniel Ekman <knegge@gmail.com>
2022-10-07 11:38:31 +0200

    Multi station scheduling
    Signed-off-by: Daniel Ekman <knegge@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -18,7 +18,19 @@ fi
 trap terminate SIGTERM
 
 while true; do
-    schedule_single_station.py -s ${SATNOGS_STATION_ID} ${SATNOGS_SCHEDULE_EXTRA}
+    if [ -n "${SATNOGS_SCHEDULE_STATIONS}" ]; then
+        echo "Running multi station.."
+        for STATION in ${SATNOGS_SCHEDULE_STATIONS}; do
+            echo "Station ${STATION}"
+            schedule_single_station.py -s ${STATION} ${SATNOGS_SCHEDULE_EXTRA}
+        done
+    elif [ -n "${SATNOGS_STATION_ID}" ]; then
+        echo "Running single station ${SATNOGS_STATION_ID}"
+        schedule_single_station.py -s ${SATNOGS_STATION_ID} ${SATNOGS_SCHEDULE_EXTRA}
+    else
+        echo "No station specified"
+        exit 1
+    fi
     sleep 3600
 done
 
